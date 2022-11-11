a23b00ce8bd069e71ae620b1f8483bc74177a91e
Raj2710 <nagarajansai2727@gmail.com>
2022-11-11 12:13:08 +0530

    removed comments

--- src/App.js
+++ src/App.js
@@ -84,21 +84,15 @@ function App() {
           </div>
           <div className="col mb-5">
             <div className="card h-100">
-              {/* <!-- Sale badge--> */}
               <div className="badge bg-dark text-white position-absolute" style={{ "top": "0.5rem", "right": "0.5rem" }}>Sale</div>
-              {/* <!-- Product image--> */}
               <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-              {/* <!-- Product details--> */}
               <div className="card-body p-4">
                 <div className="text-center">
-                  {/* <!-- Product name--> */}
                   <h5 className="fw-bolder">Sale Item</h5>
-                  {/* <!-- Product price--> */}
                   <span className="text-muted text-decoration-line-through">$50.00</span>
                   $25.00
                 </div>
               </div>
-              {/* <!-- Product actions--> */}
               <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
                 <div className="text-center">
                 {
